59338bfd0a617de39f8c93f7e5794c8ae2d90043
DyukovNA <112770025+DyukovNA@users.noreply.github.com>
2023-03-10 22:26:23 +0300

    added library wow such hacker

--- src/main/java/SizeOfFiles.java
+++ src/main/java/SizeOfFiles.java
@@ -1,6 +1,14 @@
 public class SizeOfFiles {
+    public void ReturnSizeOfFiles(String... args) {
+        for (String arg: args) {
+
+        }
+    }
+
     public static void main(String args[]) {
         System.out.println("Hell");
-
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
